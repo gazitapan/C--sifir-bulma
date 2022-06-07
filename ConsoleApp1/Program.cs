33a9b089b7d4e725cbf70e8414c515e68d765768
Abdullah Gazi Tapan <gazitapan@outlook.com>
2022-06-07 12:50:57 +0300

    C# ile sonu "0" olan sayıları bulma ve toplama

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace sifirbulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 5;
+            int[] arr = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"{i + 1}.Sayıyı Giriniz:");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int adet = 0;
+            int toplam = 0;
+
+            foreach (var item in arr)
+            {
+                if (item.ToString().EndsWith("0"))
+                {
+                    adet++;
+                    toplam += item;
+                }
+            }
+
+            Console.WriteLine($"Sonu 0 ile biten sayı adeti = {adet}");
+            Console.WriteLine($"Sonu 0 ile biten sayıların toplamı = {toplam}");
+            Console.ReadKey();
+
+        }
+    }
+}
